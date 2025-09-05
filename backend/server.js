303755c85386b3f75aca5de98375d6c500d6d71e
Shreenarayan <127131074+shreenarayan123@users.noreply.github.com>
2025-09-06 02:10:29 +0530

    final refractor

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet({
-  crossOriginResourcePolicy: false // Allow loading of resources from different origins
+  crossOriginResourcePolicy: false 
 }));
 app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:3000",
